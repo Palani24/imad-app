84bac4cdca9e935fdbef9dde6317b7e2f5f66686
Palani24 <30795248+Palani24@users.noreply.github.com>
2017-08-08 14:42:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article', function (req, res) {
-  res.send("This is article one");
+  res.sendFile(path.join(__dirname, 'ui', 'articleOne.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
