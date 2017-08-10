9ccfc48244c1e9689d0474eb78ed00e5101338dc
Palani24 <30795248+Palani24@users.noreply.github.com>
2017-08-10 17:19:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,22 @@ console.log('Loaded!');
 var counter = 0;
 var button = document.getElementById("counter");
 button.onclick = function(){
-    counter = counter+1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readystate == XMLHttpRequest.DONE){
+          if(request.status == 200){
+            counter =  request.responseText();
+            var span = document.getElementById("count");
+            span.innerHTML = counter.toString();
+          }
+      }  
+    };
+    request.open("GET","http://palani92er.imad.hasura-app.io/counter",true);
+    request.send(null);
+    
+    //counter = counter+1;
+    //var span = document.getElementById("count");
+    //span.innerHTML = counter.toString();
 };
 
 
