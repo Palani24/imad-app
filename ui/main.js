21d26bce79bd2d7704524fadf542ceed93071d57
Palani24 <30795248+Palani24@users.noreply.github.com>
2017-08-11 16:23:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,6 +21,24 @@ button.onclick = function(){
 };
 
 
+//submit
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readystate == XMLHttpRequest.DONE){
+          if(request.status == 200){
+            var counter =  request.responseText;
+            var span = document.getElementById("count");
+            span.innerHTML = counter.toString();
+          }
+      }  
+    };
+    request.open("GET","http://palani92er.imad.hasura-app.io/submitname",true);
+    request.send(null);
+    
+};
+
 var img=document.getElementById("img");
 var marginLeft = 0;
 function moveRight(){
